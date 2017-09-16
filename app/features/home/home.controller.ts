481002d1643f30d0930d03806cb4a844d80a098e
Ville M. Vainio <ville.vainio@basware.com>
2017-09-16 23:46:04 +0300

    stuff works

--- app/features/home/home.controller.ts
+++ app/features/home/home.controller.ts
@@ -1,9 +1,20 @@
-
 class HomeController {
-	message: string;
-	constructor () {
-        this.message = 'a lot to do here...';
-	}
+  message: string;
+  complex: string[];
+  constructor() {
+    this.message = "a lot to do here...";
+    this.complex = ["a", "complex", "object"];
+  }
+  changeMessage() {
+    this.message = this.message + ".";
+    console.log("click");
+  }
+
+  modifyComplex() {
+    this.complex = this.complex.concat("newline");
+    const el = document.querySelector("#mycomplex") as any;
+    el.strings = this.complex;
+  }
 }
 
 angular.module("app").controller("HomeController", HomeController);
